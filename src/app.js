71b2a60e59c19f1ee4b99c61c8da7e093fcb59a4
FrostPriest <johnericjahn@gmail.com>
2020-06-16 02:08:05 -0300

    Challenge_Complete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const project = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(project);
+
+  return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const projectIndex = repositories.findIndex(project => project.id === id);
+  if (projectIndex < 0) return response.status(400).json({ error: 'Project not found' });
+
+  const project = {
+    ...repositories[projectIndex],
+    title,
+    url,
+    techs
+  };
+
+  repositories[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const projectIndex = repositories.findIndex(project => project.id === id);
+  if (projectIndex < 0) return response.status(400).json({ error: 'Project not found' });
+
+  repositories.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const projectIndex = repositories.findIndex(project => project.id === id);
+  if (projectIndex < 0) return response.status(400).json({ error: 'Project not found' });
+
+  let { likes } = repositories[projectIndex];
+  likes++;
+
+  console.log(likes);
+
+  const project = {
+    ...repositories[projectIndex],
+    likes
+  }
+
+  repositories[projectIndex] = project;
+
+  response.json(project);
 });
 
 module.exports = app;
